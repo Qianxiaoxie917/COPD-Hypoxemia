f175985b1ce6992764c8c1f5af9c90a48b2fc041
Qian Fang <56874231+Qianxiaoxie917@users.noreply.github.com>
2023-07-17 11:32:35 +0800

    mimiciv_copd_icd_codes.sql

--- Data_preprocessing/MIMICIV/mimiciv_copd_icd_codes.sql
+++ Data_preprocessing/MIMICIV/mimiciv_copd_icd_codes.sql
@@ -10,7 +10,7 @@ FROM `oxgenator.mimiciv_hosp.diagnoses_icd` AS icd)
 SELECT
 icd_presence.hadm_id AS hadm_id,
 
-COUNT(CASE WHEN icd_presence.icd_code  LIKE '%J4%' or icd_presence.icd_num BETWEEN 490 AND 496  THEN 1 END) > 0 AS has_copd_disease,
+COUNT(CASE WHEN icd_presence.icd_code  LIKE 'J4%' or icd_presence.icd_num BETWEEN 490 AND 496  THEN 1 END) > 0 AS has_copd_disease,
 FROM icd_presence
 GROUP BY icd_presence.hadm_id
 
